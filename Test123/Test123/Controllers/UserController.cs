400da5b2b258da3391ff0734183c20f5547937aa
kasireddy26 <arunkasireddy@Aruns-MBP.fios-router.home>
2017-04-11 12:31:10 -0500

    work

---
+++ Test123/Test123/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+//using System.Web.Mvc;
+using Test123.Models;
+
+
+namespace Test123.Controllers
+{
+    public class UserController : ApiController
+    {
+        // GET: User
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        List<UserInformation> UserInformations = new List<UserInformation>();
+
+        [HttpGet] //GET api/userInformation
+        public IEnumerable<UserInformation> GetAllUserInformations()
+        {
+            GetUserInformations();
+            return UserInformations;
+        }
+
+        private void GetUserInformations()
+        {
+            UserInformations.Add(new UserInformation {  userName = "Robert", address = "NewYork", age = "30" });
+            UserInformations.Add(new UserInformation { userName = "William", address = "NewYork", age = "31" });
+            UserInformations.Add(new UserInformation { userName = "Eric", address = "Texas", age = "32" });
+            UserInformations.Add(new UserInformation { userName = "Pauline", address = "Texas", age = "33" });
+            UserInformations.Add(new UserInformation { userName = "Chris", address = "Dallas", age = "34" });
+           
+        }
+        [HttpGet] //GET api/GetUserInformations
+        public IEnumerable<UserInformation> GetUserInformations(string selectedUserName)
+        {
+            if (UserInformations.Count() > 0)
+            {
+                return UserInformations.Where(p => p.userName == selectedUserName);
+            }
+            else
+            {
+                GetUserInformations();
+                return UserInformations.Where(p => p.userName == selectedUserName);
+            }
+        }
+
+    }
+}
---
+++ Test123/Test123/Models/UserInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test123.Models
+{
+    public class UserInformation
+    {
+        public string userName { get; set; }
+        public string address { get; set; }
+        public string age { get; set; }
+
+    }
+}
